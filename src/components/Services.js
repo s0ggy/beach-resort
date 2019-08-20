2a12c90636c93cb24710f60e979d30f485c8c638
Sone <sthaya@gmail.com>
2019-08-19 19:13:24 -0700

    added services component and title component

---
+++ src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+import Title from './Title';
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: 'Free Cocktails',
+        info:
+          'lorem ipsum dolor sit amet consectetur adipisicing elit. Magin, corpois!'
+      },
+      {
+        icon: <FaHiking />,
+        title: 'Enless Hiking',
+        info:
+          'lorem ipsum dolor sit amet consectetur adipisicing elit. Magin, corpois!'
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: 'Free Shuttle',
+        info:
+          'lorem ipsum dolor sit amet consectetur adipisicing elit. Magin, corpois!'
+      },
+      {
+        icon: <FaBeer />,
+        title: 'Strongest Beer',
+        info:
+          'lorem ipsum dolor sit amet consectetur adipisicing elit. Magin, corpois!'
+      }
+    ]
+  };
+  render() {
+    return (
+      <section className='services'>
+        <Title title='services' />
+        <div className='services-center'>
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className='service'>
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
