d0dd8787f178a0129f64637bc0c69142801e1415
Sone <sthaya@gmail.com>
2019-08-24 18:21:01 -0700

    modified roomContainer component

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import RoomsFilter from './RoomsFilter.js';
 import RoomsList from './RoomsList';
+import { withRoomConsumer } from '../context';
+import Loading from './Loading';
 
-export default function RoomsContainer() {
+function RoomsContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div>
-      Hello from Rooms Container
-      <RoomsFilter />
-      <RoomsList />
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
     </div>
   );
 }
+
+export default withRoomConsumer(RoomsContainer);
+
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from Rooms Container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
