f2a28c5e74fcb01528fd799478725d84f48f8fce
Sone <sthaya@gmail.com>
2019-08-25 21:33:50 -0700

    worked on filtering by capacity

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -28,10 +28,22 @@ export default function RoomsFilter({ rooms }) {
   types = ['all', ...types];
   // map to jsx
   types = types.map((item, index) => {
-    return <option value={item} key={index}>
-      {item}
-    </option>
-  })
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  let people = getUnique(rooms, 'capacity');
+
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
 
   return (
     <section className='filter-container'>
@@ -51,6 +63,20 @@ export default function RoomsFilter({ rooms }) {
           </select>
         </div>
         {/* END SELECT TYPE */}
+        {/* guests */}
+        <div className='form-group'>
+          <label htmlfor='capacity'>Guests</label>
+          <select
+            name='capacity'
+            id='capacity'
+            value={capacity}
+            className='form-control'
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+        {/* END guests */}
       </form>
     </section>
   );
