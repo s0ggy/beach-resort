ef25ca3b979eae17f5dc6f788fdc7604424e61db
Sone <sthaya@gmail.com>
2019-08-25 18:45:29 -0700

    worked on filtered room component, search by room type

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,5 +1,57 @@
 import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
+// get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
 
-export default function RoomsFilter() {
-  return <div>Hello from Rooms Filter</div>;
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  // get unique types
+  let types = getUnique(rooms, 'type');
+  // add all
+  types = ['all', ...types];
+  // map to jsx
+  types = types.map((item, index) => {
+    return <option value={item} key={index}>
+      {item}
+    </option>
+  })
+
+  return (
+    <section className='filter-container'>
+      <Title title='search rooms' />
+      <form className='filter-form'>
+        {/* SELECT TYPE */}
+        <div className='form-group'>
+          <label htmlfor='type'>room type</label>
+          <select
+            name='type'
+            id='type'
+            value={type}
+            className='form-control'
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* END SELECT TYPE */}
+      </form>
+    </section>
+  );
 }
